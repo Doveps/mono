00d1982f67fdd39524fe2117459805eb23be1188
Kurt Yoder <kyoder@doveps.com>
2014-12-06 16:31:00 -0500

    Parse and record processes

--- parser/log_file/processes_stdout.py
+++ parser/log_file/processes_stdout.py
@@ -1,4 +1,35 @@
 from . import common
+from systems import process
 
 class ProcessesStdoutLog(common.Log):
-    pass
+
+    def parse(self):
+        self.logger.debug('parsing')
+
+        self.processes = {}
+
+        # since processes can overlap, resulting in >1 process in a
+        # self.processes key, manually keep track of the count:
+        self.process_count = 0
+
+        with open(self.path, 'r') as f:
+            for line_number, line in enumerate(f.readlines()):
+                # ignore header lines
+                if line_number < 1: continue
+
+                self.process_count += 1
+                self.parse_line(line)
+
+    def parse_line(self, line):
+        parts = line.split()
+
+        command = ' '.join(parts[9:])
+
+        if not self.processes.has_key(command):
+            self.processes[command] = process.Process(command)
+
+        self.processes[command].add(parts[0:9])
+
+    def record(self, flavor):
+        self.logger.debug('recording %d processes',self.process_count)
+        flavor.record('processes', self.processes)
---
+++ systems/process.py
@@ -0,0 +1,14 @@
+class ProcessInstance(object):
+    def __init__(self, fields):
+        (self.pid, self.ppid, self.uid, self.gid, self.cgroup,
+                self.flag, self.nice, self.priority,
+                self.tty) = fields[0:9]
+
+class Process(object):
+    def __init__(self, command):
+        self.command = command
+        self.instances = []
+
+    def add(self, fields):
+        self.instances.append(ProcessInstance(fields))
+
