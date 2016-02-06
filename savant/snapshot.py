0f25a2a0f76370642e358db9b45b4f369fd5cf1d
Kurt Yoder <kyoder@doveps.com>
2016-02-06 12:20:45 -0500

    Rename OS to Snapshot
    OS will be a different type of node, linking to snapshots.

--- savant/snapshot.py
+++ savant/snapshot.py
@@ -7,15 +7,16 @@ from . import db
 
 g = db.get_db()
 
-class OS(object):
-    '''All systems link to an instance of an OS.'''
+class Snapshot(object):
+    '''A system snapshot contains links to all sub-systems, e.g.
+    packages, files, etc.'''
 
     def __init__(self, id):
         self.id = id
-        self.n = Node('OS', id=self.id)
+        self.n = Node('Snapshot', id=self.id)
 
     def add(self, obj):
-        '''Link a new object to this OS.'''
+        '''Link a new object to this snapshot.'''
         o = obj.get_node()
         contains = Relationship(self.n, 'contains', o)
         g.graph.create(contains)
