7f62ba23608598c442542639b68c6a11c0436702
JosiahRegencia <jawshaeleazar@gmail.com>
2017-07-24 23:32:36 +0800

    Initiated Flask script

--- savant/models.py
+++ savant/models.py
@@ -1,29 +1,34 @@
-from django.db import models
+from sqlalchemy import create_engine
+import os
+
+connection = 'local' #or change it to local if you're running on local machine
+
+if connection == 'local':
+	class DBconn:
+	    def __init__(self):
+	        engine = create_engine("postgresql://anoncare:anoncare@127.0.0.1:5432/acdbfinal")
+	        self.conn = engine.connect()
+	        self.trans = self.conn.begin()
 
 
-class Comparison(models.Model):
-    """A Comparison is a collection of diffs, for example as generated and
-        exported by the bassist."""
-    diffs = models.ManyToManyField("Diff")
+	    def getcursor(self):
+	        cursor = self.conn.connection.cursor()
+	        return cursor
 
 
-class Diff(models.Model):
-    # Hashed so it can't get added twice
-    content = models.CharField(max_length=250)
-    system = models.CharField(max_length=250)
-    action = models.CharField(max_length=250)
-    name = models.CharField(max_length=250)
+	    def dbcommit(self):
+	        self.trans.commit()
 
-    def __str__(self):
-        return "%s|%s|%s" % (self.action, self.system, self.name)
+	class DBconn:
+	    def __init__(self):
 
+	        engine = create_engine(database)
+	        self.conn = engine.connect()
+	        self.trans = self.conn.begin()
 
-class Set(models.Model):
-    """A Set is one or more diffs from a Comparison object. Diffs result from a
-        change made to an OS. For example: installing a package."""
-    comparison = models.ForeignKey("Comparison")
-    diffs = models.ManyToManyField("Diff")
+	    def getcursor(self):
+	        cursor = self.conn.connection.cursor()
+	        return cursor
 
-
-class Playbook(models.Model):
-    pass
+	    def dbcommit(self):
+	        self.trans.commit()
