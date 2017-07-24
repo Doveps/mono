ea82c0aa52c0ab18f323b60379fab38adbfa10bd
JosiahRegencia <jawshaeleazar@gmail.com>
2017-07-24 23:59:29 +0800

    Added run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+
+from savant import savant
+
+
+if __name__ == '__main__':
+    savant.run()
---
+++ savant/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+
+savant = Flask(__name__)
+savant.debug = True
+
+
+# from app import views
