06ef7c4de861172c9a5aa60e61caf4a5daf88996
JosiahRegencia <jawshaeleazar@gmail.com>
2017-07-24 23:42:32 +0800

    Scripts for DB Connection and Stored Procedures

--- savant/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SavantConfig(AppConfig):
-    name = 'savant'
--- savant/managers/ansible/role_dirs/files_dir.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2015 Kurt Yoder
-# See the file LICENSE for copying permission.
-from . import common
-
-class Files(common.Directory):
-    required_file = None
