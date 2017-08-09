6fef1229005fe7c38f5f418354840da1c59dddcc
JosiahRegencia <jawshaeleazar@gmail.com>
2017-08-10 06:20:15 +0800

    Removed more debugging code

--- savant/app/run_sql.py
+++ savant/app/run_sql.py
@@ -7,6 +7,5 @@ def execute_sql(sql_file):
 	cursor = connection.cursor()
 
 	os.chdir("/home/josiah/Documents/Doveps/mono/savant/db_scripts")
-	print "directory: ", os.getcwd()
 	cursor.execute(open(str(sql_file), "r").read())
 	connection.commit()
