9516027b520ac72669547d0f489828af43a0ca0c
JosiahRegencia <jawshaeleazar@gmail.com>
2017-08-10 06:18:28 +0800

    Removed debugging code

--- savant/app/run_sql.py
+++ savant/app/run_sql.py
@@ -5,18 +5,8 @@ import os
 def execute_sql(sql_file):
 	connection = psycopg2.connect("dbname='Doveps' user='postgres' host='localhost' password='postgres'")
 	cursor = connection.cursor()
-	print "connected?"
-
-	cursor.execute("""SELECT table_name FROM information_schema.tables
-       WHERE table_schema = 'public'""")
-	for table in cursor.fetchall():
-		print "prev: ", table
 
 	os.chdir("/home/josiah/Documents/Doveps/mono/savant/db_scripts")
 	print "directory: ", os.getcwd()
 	cursor.execute(open(str(sql_file), "r").read())
-	cursor.execute("""SELECT table_name FROM information_schema.tables
-       WHERE table_schema = 'public'""")
-	for table in cursor.fetchall():
-		print "now: ", table
 	connection.commit()
