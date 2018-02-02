c65ca95cd905c13f0e7ecf19da67d68aa61987b5
JosiahRegencia <jawshaeleazar@gmail.com>
2018-02-02 22:42:26 +0800

    query edits applied to unittests

--- savant/tests/test_basic.py
+++ savant/tests/test_basic.py
@@ -6,8 +6,9 @@ from app.results import create_flavors, compare
 from app import query
  
 class TestDoveps(unittest.TestCase):
-    query.execute_sql("tables.sql")
-    query.execute_sql("functions.sql")
+    que_test = query.Query()
+    que_test.execute_sql("tables.sql")
+    que_test.execute_sql("functions.sql")
 
     def test_acreate_flavor(self):
         path = str(os.getcwd()).split("/mono", 1)[0]
