27f744af1a7c98e6fcd63e1b7e048f5faa47f501
Josiah Eleazar T. Regencia <jawshaeleazar@gmail.com>
2018-02-12 12:35:04 +0800

    Null testcases (#88)
    * test case for null cases
    
    * Changed db name
    
    * Changed db name in test

--- savant/tests/test_basic.py
+++ savant/tests/test_basic.py
@@ -4,6 +4,7 @@ from StringIO import StringIO
 from app import app
 from app.results import create_flavors, compare
 from app import query
+import psycopg2
  
 class TestDoveps(unittest.TestCase):
 
@@ -30,6 +31,20 @@ class TestDoveps(unittest.TestCase):
             'file': (StringIO('My inputs'),(path + '/mono/savant/tests/Scanner_Files/33.33.33.51/find_users_stdout.log'))
         })
         self.assertEqual(response.status_code, 200)
- 
+
+    def test_nullcases(self):
+        engine_name = "postgresql://postgres:postgres@127.0.0.1:5432/travis_ci_test"
+        conn = psycopg2.connect(engine_name)
+        cur = conn.cursor()
+
+        cur.execute("select store_debs(%s, %s ,%s, %s)", ('stat', None, 'test', 'test'))
+        conn.commit()
+        cur.execute("select store_debs(%s, %s ,%s, %s)", ('stat', None, 'test', 'test'))
+        conn.commit()
+        cur.execute("select count(*) from debs where stat=%s and name is null and version=%s and architecture=%s", ('stat', 'test', 'test'))
+        count = cur.fetchone()
+        self.assertEqual(count[0], 1)
+
+
 if __name__ == '__main__':
     unittest.main()
