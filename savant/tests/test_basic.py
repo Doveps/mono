8b7d895d57aa9462934a3a160caae065c0d689bb
JosiahRegencia <jawshaeleazar@gmail.com>
2018-03-08 22:48:35 +0800

    Removed create in compare test

--- savant/tests/test_basic.py
+++ savant/tests/test_basic.py
@@ -4,7 +4,8 @@ from StringIO import StringIO
 from app import app
 from app.results import create_flavors, compare
 from app import query
-import psycopg2, json
+import psycopg2, json, sqlalchemy
+from sqlalchemy import create_engine
  
 class TestDoveps(unittest.TestCase):
 
@@ -23,14 +24,6 @@ class TestDoveps(unittest.TestCase):
     def test_compare(self):
         path = str(os.getcwd()).split("/mono", 1)[0]
         tester = app.test_client(self)
-
-	response = tester.post('/doveps/api/flavor/create/',
-        data = {
-            'file': (StringIO('My inputs'),(path + '/mono/savant/tests/Scanner_Files/33.33.33.50/find_debs_stdout.log')),
-            'file': (StringIO('My inputs'),(path + '/mono/savant/tests/Scanner_Files/33.33.33.50/find_groups_stdout.log')),
-            'file': (StringIO('My inputs'),(path + '/mono/savant/tests/Scanner_Files/33.33.33.50/find_shadow_stdout.log')),
-            'file': (StringIO('My inputs'),(path + '/mono/savant/tests/Scanner_Files/33.33.33.50/find_users_stdout.log'))
-        })
 
         response = tester.post('/doveps/api/flavor/compare/',
         data = {
